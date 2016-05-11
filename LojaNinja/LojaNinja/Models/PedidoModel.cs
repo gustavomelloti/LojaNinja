52e6d4bf1704633dc18482d702b1749160ae61cb
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-05-11 17:21:21 -0300

    feat(c#): publicar primeira versão do cadastro.

---
+++ LojaNinja/LojaNinja/Models/PedidoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.Models
+{
+    public class PedidoModel
+    {
+        public DateTime DataDesejoEntrega { get; set; }
+        public string NomeProduto { get; set; }
+        public double ValorVenda { get; set; }
+        public TipoPagamento TipoPagamento { get; set; }
+        public string NomeCliente { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
