4366186d498501d5327e759db185c190c358be2b
Arthur Tassinari Cabral <Arthur Tassinari Cabral>
2016-05-11 22:05:30 -0300

    feat(LojaNinja): Adicionar página de cadastro.

--- LojaNinja/LojaNinja/Models/PedidoModel.cs
+++ LojaNinja/LojaNinja/Models/PedidoModel.cs
@@ -8,17 +8,31 @@ namespace LojaNinja.Models
 {
     public class PedidoModel
     {
-        [Required(ErrorMessage ="Informe a data de desejo.")]
+        [Display(Name = "Data de entrega: ")]
+        [Required(ErrorMessage ="Informe a data de entrega.")]
         public DateTime DataDesejoEntrega { get; set; }
 
+        [Display(Name = "Nome do produto: ")]
         [Required(ErrorMessage = "Informe o nome do produto.")]
         public string NomeProduto { get; set; }
 
+        [Display(Name = "Valor da venda: ")]
         [Required(ErrorMessage = "Informe o valor da venda.")]
-        public double ValorVenda { get; set; }
-        public TipoPagamento TipoPagamento { get; set; }
+        public string ValorVenda { get; set; }
+
+        [Display(Name = "Escolha um tipo de pagamento: ")]
+        public TipoPagamento TipoDePagamento { get; set; }
+
+        [Display(Name = "Nome: ")]
+        [Required(ErrorMessage = "Informe seu nome.")]
         public string NomeCliente { get; set; }
+
+        [Display(Name = "Cidade: ")]
+        [Required(ErrorMessage = "Informe a cidade.")]
         public string Cidade { get; set; }
+
+        [Display(Name = "Estado: ")]
+        [Required(ErrorMessage = "Informe o estado.")]
         public string Estado { get; set; }
     }
 }
