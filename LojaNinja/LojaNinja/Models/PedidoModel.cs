8e16b0391de13a4cb9c89576d898cad4c764382c
Douglas Ballester <douglas.ballester@cwi.com>
2016-05-11 23:01:03 -0300

    feat(Detalhes): adicionar view de detalhes(em andamento)

--- LojaNinja/LojaNinja/Models/PedidoModel.cs
+++ LojaNinja/LojaNinja/Models/PedidoModel.cs
@@ -8,30 +8,30 @@ namespace LojaNinja.Models
 {
     public class PedidoModel
     {
-        [Display(Name = "Data de entrega: ")]
+        [Display(Name = "Data de entrega ")]
         [Required(ErrorMessage ="Informe a data de entrega.")]
         public DateTime DataDesejoEntrega { get; set; }
 
-        [Display(Name = "Nome do produto: ")]
+        [Display(Name = "Nome do produto ")]
         [Required(ErrorMessage = "Informe o nome do produto.")]
         public string NomeProduto { get; set; }
 
-        [Display(Name = "Valor da venda: ")]
+        [Display(Name = "Valor da venda ")]
         [Required(ErrorMessage = "Informe o valor da venda.")]
         public string ValorVenda { get; set; }
 
-        [Display(Name = "Escolha um tipo de pagamento: ")]
+        [Display(Name = "Escolha um tipo de pagamento ")]
         public TipoPagamento TipoDePagamento { get; set; }
 
-        [Display(Name = "Nome: ")]
+        [Display(Name = "Nome ")]
         [Required(ErrorMessage = "Informe seu nome.")]
         public string NomeCliente { get; set; }
 
-        [Display(Name = "Cidade: ")]
+        [Display(Name = "Cidade ")]
         [Required(ErrorMessage = "Informe a cidade.")]
         public string Cidade { get; set; }
 
-        [Display(Name = "Estado: ")]
+        [Display(Name = "Estado ")]
         [Required(ErrorMessage = "Informe o estado.")]
         public string Estado { get; set; }
     }
